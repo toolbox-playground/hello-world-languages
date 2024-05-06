b5dade12a0900a63f56a84831ac279f2fc8ead21
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-05-06 09:30:39 -0300

    comentarios

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,7 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); // Cria uma instância do WebApplicationBuilder usando os argumentos fornecidos
 
-app.UseStaticFiles();
-app.MapGet("/", () => "Bem-Vindo ao Hello World .Net Asp!");
+var app = builder.Build(); // Constrói a aplicação web com base na instância do WebApplicationBuilder
 
-app.Run();
+app.UseStaticFiles(); // Configura a aplicação para usar arquivos estáticos, como arquivos CSS, JavaScript e imagens
+
+app.MapGet("/", () => "Bem-Vindo ao Hello World .Net Asp!"); // Mapeia a rota raiz ("/") para uma função anônima que retorna a mensagem "Bem-Vindo ao Hello World .Net Asp!"
+
+app.Run(); // Executa a aplicação web
